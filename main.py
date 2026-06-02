d4c5878ea1b97a1c026f7a41ba924a7aab9d1e04
Antony Schmitt <132669411+SchmittAntony@users.noreply.github.com>
2026-06-01 21:26:21 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import pygame as pg
+
+print('Setup Start')
+pg.init()
+window = pg.display.set_mode(size=(600, 480))
+print('Setup End')
+
+print('Loop Start')
+while True:
+    # Check for all events
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pygame.quit()  # Close Window
+            quit()  # end pygame
